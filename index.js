090c235e627cd1a795bb8913d40ac3fa6a040fe2
Richard Willars <richard@richardwillars.com>
2016-09-19 10:52:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ class SonosDriver {
 
 	setEventEmitter(eventEmitter) {
 		this.eventEmitter = eventEmitter;
-		//when something happens with this speaker you can emit an event to let the homebox platform know:
+		//when something happens with this speaker you can emit an event to let the devlator platform know:
 		//it should only emit events which are valid types (see documentation on speakers for more info)
 		//this.eventEmitter.emit('eventType','driverId','deviceId','value')
 
